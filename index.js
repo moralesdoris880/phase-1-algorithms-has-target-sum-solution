2fed30361f178d09d6cb6600c48d1a482dd2dcc6
Doris Morales <moralesdoris880@gmail.com>
2021-11-11 17:42:23 -0500

    commit tree 006a958be272fa0c16476f3f45c3e765fa5bc5bb

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  // 1 step
+  const seenNumbers = {};
+  for (const number of array) {
+    // n steps
+    const complement = target - number;
+    // n steps
+    if (seenNumbers[complement]) return true;
+    // n steps
+    seenNumbers[number] = true;
+  }
+  // 1 step
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  0 (n)
 */
 
 /* 
-  Add your pseudocode here
+  Add your pseudocode here:
+  1. Takes two arguements 
+  1.5. Make an empty object
+  2. Iterate thru array 
+  3. Make a variable that holds the number that complements the target number
+  4. If statement that checks if it is in the object, if not it will add it
 */
 
 /*
-  Add written explanation of your solution here
+  Add written explanation of your solution here:
+  Returns true or false based on if the 
+  array has two numbers that equal to the target number
+
 */
 
 // You can run `node index.js` to view these console logs
